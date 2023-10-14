f28fe89b3d737dc25f68a52a72a7cccf7f782872
Parth-88 <112095322+Parth-88@users.noreply.github.com>
2023-10-14 22:19:52 +0530

    Update Parth 2 (aggressiveCows).cpp

--- Parth 2 (aggressiveCows).cpp
+++ Parth 2 (aggressiveCows).cpp
@@ -5,25 +5,25 @@
 using namespace std;
 
 bool canWePlace(vector<int> &stalls, int dist, int cows) {
-    int n = stalls.size(); //size of array
-    int cntCows = 1; //no. of cows placed
-    int last = stalls[0]; //position of last placed cow.
+    int n = stalls.size(); 
+    int cntCows = 1;
+    int last = stalls[0];
     for (int i = 1; i < n; i++) {
         if (stalls[i] - last >= dist) {
-            cntCows++; //place next cow.
-            last = stalls[i]; //update the last location.
+            cntCows++; 
+            last = stalls[i]; 
         }
         if (cntCows >= cows) return true;
     }
     return false;
 }
 int aggressiveCows(vector<int> &stalls, int k) {
-    int n = stalls.size(); //size of array
-    //sort the stalls[]:
+    int n = stalls.size();
+   
     sort(stalls.begin(), stalls.end());
 
     int low = 1, high = stalls[n - 1] - stalls[0];
-    //apply binary search:
+   
     while (low <= high) {
         int mid = (low + high) / 2;
         if (canWePlace(stalls, mid, k) == true) {
